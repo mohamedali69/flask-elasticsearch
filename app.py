b4f3c03d4c31dfd24ef0509034ff4724beb59534
Mohamed Ali Chakhari <mohamed.ali.chakhari@neopolis-dev.com>
2024-11-13 22:24:43 +0100

    update app py

--- app.py
+++ app.py
@@ -21,10 +21,6 @@ class Product(db.Model):
     name = db.Column(db.String(50))
     description = db.Column(db.String(200))
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 @app.route("/index", methods=["POST"])
 def index_data():
     bulk_index_data_async.delay()
@@ -93,3 +89,9 @@ def search():
 def health_check():
     health = es.cluster.health()
     return jsonify({"status": health["status"], "details": health})
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run()
